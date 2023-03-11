5892635d19a8cf01689f7cd8dcc8fe01345db286
Lander Joshua Vitug <lander.vitug@gmail.com>
2023-03-11 17:31:23 -0500

    reuse _update_prop in HandlerPolyCollection for _default_update_prop in HandlerPatchCollection

--- matplotlib-main/lib/matplotlib/legend_handler.py
+++ matplotlib-main/lib/matplotlib/legend_handler.py
@@ -820,14 +820,33 @@ class HandlerPatchCollection(HandlerPatch):
     Handler for `.PatchCollection` instances.
     """
     def _default_update_prop(self, legend_handle, orig_handle):
-        facecolor = orig_handle.get_facecolor()[0]
-        edgecolor = orig_handle.get_edgecolor()[0]
-        linewidth = orig_handle.get_linewidths()[0]
-        hatch = orig_handle.get_hatch()[0]
-        legend_handle.set_facecolor(facecolor)
-        legend_handle.set_edgecolor(edgecolor)
-        legend_handle.set_linewidth(linewidth)
-        legend_handle.set_hatch(hatch)
+        def first_color(colors):
+            if colors.size == 0:
+                return (0, 0, 0, 0)
+            return tuple(colors[0])
+
+        def get_first(prop_array):
+            if len(prop_array):
+                return prop_array[0]
+            else:
+                return None
+
+        # orig_handle is a PatchCollection and legend_handle is a Patch.
+        # Directly set Patch color attributes (must be RGBA tuples).
+        legend_handle._facecolor = first_color(orig_handle.get_facecolor())
+        legend_handle._edgecolor = first_color(orig_handle.get_edgecolor())
+        legend_handle._original_facecolor = orig_handle._original_facecolor
+        legend_handle._original_edgecolor = orig_handle._original_edgecolor
+        legend_handle._fill = orig_handle.get_fill()
+        legend_handle._hatch = orig_handle.get_hatch()
+        # Hatch color is anomalous in having no getters and setters.
+        legend_handle._hatch_color = orig_handle._hatch_color
+        # Setters are fine for the remaining attributes.
+        legend_handle.set_linewidth(get_first(orig_handle.get_linewidths()))
+        legend_handle.set_linestyle(get_first(orig_handle.get_linestyles()))
+        legend_handle.set_transform(get_first(orig_handle.get_transforms()))
+        legend_handle.set_figure(orig_handle.get_figure())
+        # Alpha is already taken into account by the color attributes.
     
     def create_artists(self, legend, orig_handle,
                        xdescent, ydescent, width, height, fontsize, trans):
